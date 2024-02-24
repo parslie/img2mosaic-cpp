8c89fba764afd111c5df86d467abcff28fef044c
Viktor Holta <viktor.holta@outlook.com>
2024-02-24 15:32:27 +0100

    Init project

---
+++ img2mosaic.cpp
@@ -0,0 +1,12 @@
+﻿// img2mosaic.cpp : Defines the entry point for the application.
+//
+
+#include "img2mosaic.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
