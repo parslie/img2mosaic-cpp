61cbbe4f3bc07432790c68484531ece3d6608da9
Viktor Holta <viktor.holta@outlook.com>
2024-07-02 17:06:47 +0200

    Get image paths for analysis

--- src/commands/analysis.cpp
+++ src/commands/analysis.cpp
@@ -1,6 +1,61 @@
 #include "commands.hpp"
 
+#include <filesystem>
+#include <iostream>
+
+namespace fs = std::filesystem;
+
+static bool is_img_file(const fs::path &img_path)
+{
+    constexpr std::string_view EXTS[]{ ".png", ".jpg", ".jpeg", ".webp" };
+    for (auto ext : EXTS)
+        if (ext == img_path.extension())
+            return true;
+    return false;
+}
+
+static bool is_valid_img_entry(const fs::directory_entry &entry)
+{
+    try
+    {
+        // Fails on Windows if the file name is not valid
+        std::string path{ entry.path().string() };
+    }
+    catch (...)
+    {
+        return false;
+    }
+
+    return is_img_file(entry.path());
+} 
+
+static std::vector<fs::path> get_img_paths(const fs::path &dir_path, const bool recurse)
+{
+    std::vector<fs::path> img_paths{};
+
+    if (recurse)
+    {
+        for (auto entry : fs::recursive_directory_iterator(dir_path))
+            if (is_valid_img_entry(entry))
+                img_paths.push_back(entry.path());
+    }
+    else
+    {
+        for (auto entry : fs::directory_iterator(dir_path))
+            if (is_valid_img_entry(entry))
+                img_paths.push_back(entry.path());
+    }
+
+    return img_paths;
+}
+
 int analyze(const Arguments &args)
 {
+    constexpr const char *TAB{ "   " };
+
+    std::cout << "[Initializing analysis...]" << '\n';
+    const std::vector<fs::path> img_paths{ get_img_paths(args.analysis_args.dir_path, args.analysis_args.recurse) };
+    std::cout << TAB << "Loaded image paths." << '\n';
+
     return EXIT_SUCCESS;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include "arguments.hpp"
 #include "commands/commands.hpp"
 
+Arguments debug_analysis_args(int argc, char **argv)
+{
+    const int debug_argc = 4;
+    const char *const_debug_argv[debug_argc]{ argv[0], "saved_pics", "analyze", "E:\\Pictures\\Saved Pictures" };
+    char **debug_argv = const_cast<char **>(const_debug_argv);
+    return Arguments{ debug_argc, debug_argv };
+}
+
 int main(int argc, char **argv)
 {
-    const Arguments args{ argc, argv };
+    //const Arguments args{ argc, argv };
+    const Arguments args{ debug_analysis_args(argc, argv) };
 
     if (args.generation_args.parsed)
         return generate(args);
