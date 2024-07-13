39dfd305e33a135ce3e5a112a845f1c790215d71
Viktor Holta <viktor.holta@outlook.com>
2024-07-13 15:51:32 +0200

    Prepare generation command

--- src/commands/generation.cpp
+++ src/commands/generation.cpp
@@ -1,6 +1,33 @@
 #include "commands.hpp"
 
+#include "../data/image.hpp"
+#include "../data/palette.hpp"
+
+#include <iostream>
+
 int generate(const Arguments &args)
 {
+    constexpr const char *TAB{ "   " };
+
+    std::cout << "[Preparing generation...]" << '\n';
+    std::shared_ptr<ImageLoader> img_loader{ std::make_shared<ImageLoader>() };
+    Palette palette{ args.profile, img_loader };
+    std::cout << TAB << "Loaded palette." << '\n';
+
+    Image src_img{ args.generation_args.src_path };
+    src_img.scale_to_cover(args.generation_args.src_size);
+    std::cout << TAB << "Loaded source image." << '\n';
+
+    unsigned int dst_width{ src_img.width() * args.generation_args.pixel_size };
+    unsigned int dst_height{ src_img.height() * args.generation_args.pixel_size };
+    Image dst_img{ dst_width, dst_height, args.generation_args.dst_path };
+    std::cout << TAB << "Loaded destination image." << '\n';
+
+    std::cout << "[Starting generation...]" << '\n';
+    src_img.show("src");
+    dst_img.show("dst");
+
+    std::cout << "[Saving mosaic...]" << '\n';
+
     return EXIT_SUCCESS;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,19 @@ Arguments debug_analysis_args(int argc, char **argv)
     return Arguments{ debug_argc, debug_argv };
 }
 
+Arguments debug_generation_args(int argc, char **argv)
+{
+    const int debug_argc = 9;
+    const char *const_debug_argv[debug_argc]{ argv[0], "saved_pics", "generate", "-s", "64", "-p", "64", "E:\\Pictures\\Saved Pictures\\20220123_101647.jpg", "..\\test.jpg"};
+    char **debug_argv = const_cast<char **>(const_debug_argv);
+    return Arguments{ debug_argc, debug_argv };
+}
+
 int main(int argc, char **argv)
 {
     //const Arguments args{ argc, argv };
-    const Arguments args{ debug_analysis_args(argc, argv) };
+    //const Arguments args{ debug_analysis_args(argc, argv) };
+    const Arguments args{ debug_generation_args(argc, argv) };
 
     if (args.generation_args.parsed)
         return generate(args);
